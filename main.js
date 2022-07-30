3da59a64b5091cd1543411e3fb6d47cf16c7a685
Jumbled-E <96026179+Jumbled-E@users.noreply.github.com>
2022-07-30 12:27:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -32,4 +32,60 @@ window.addEventListener("keydown", my_keydown);
 function my_keydown(e){
     keyPressed=e.keyCode;
     console.log(keyPressed);
+    if(keyPressed=='38'){
+        up();
+        console.log("arriba");
+    }
+    if(keyPressed=="40"){
+        down();
+        console.log("abajo");
+    
+    }
+if(keyPressed=="39"){
+right();
+console.log("derecha");
+
+}
+
+if(keyPressed=="37"){
+    left();
+    console.log("izquierda")
+
+}
+
+
+}
+
+function up(){
+    if(rover_y>=0){
+        rover_y = rover_y - 10;
+        uploadBackground();
+        uploadRover();
+    }
+
+}
+function down(){
+    if(rover_y<=500){
+        rover_y = rover_y + 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+function left(){
+    if(rover_x>=0){
+        rover_x = rover_x - 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+
+
+function right(){
+    if(rover_x<=500){
+        rover_x = rover_x + 10;
+        uploadBackground();
+        uploadRover();
+    }
 }
